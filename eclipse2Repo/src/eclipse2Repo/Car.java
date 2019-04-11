ace2525b0c3b3547d9a51b4b1f20452466304517
jwangggg <31377430+jwangggg@users.noreply.github.com>
2019-04-11 11:15:29 -0700

    Update Car.java
    change car

--- eclipse2Repo/src/eclipse2Repo/Car.java
+++ eclipse2Repo/src/eclipse2Repo/Car.java
@@ -5,6 +5,6 @@ public class Car {
 	public void printName() {
 		System.out.println("Class name is car.");
 		System.out.println("add some new feature.");
-		//some changes in master
+		System.out.println("changes");
 	}
 }
